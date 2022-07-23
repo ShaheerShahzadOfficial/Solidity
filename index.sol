7aeb83aacae585de2712e0facdd2c4fd9a4e9dc0
Shaheer Shahzad Official <mohammedshaheershahzadrawda5@gmail.com>
2022-07-23 12:46:40 +0500

    Update index.sol

--- index.sol
+++ index.sol
@@ -65,7 +65,38 @@ function divide(uint8 _a,uint8 _b) public  returns(uint8){
 
 
 
+/// task 2 
 
+
+ SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+
+contract newContract {
+    
+    struct student{
+        string name;
+        string parentName;
+        address id;
+    }
+    mapping (address => student) students;
+    address public owner;
+
+
+
+
+    event studentReg(string,string,address);
+    function getStruct (string memory _name, string memory _parentName, address _id) public {
+        student memory Student = student(_name,_parentName, _id);
+        students[_id] = Student;
+    }
+    function getStudentData (address _id) public view returns(student memory){
+        student memory stdData = students[_id];
+        return stdData;
+    }
+}
 
 
 
